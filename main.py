8aaa304ead2049d46eab90eb661c14c3b04b4e43
Adrien <bouzekoadrienzangue@gmail.com>
2023-02-12 18:38:25 +0100

    calculator time

---
+++ main.py
@@ -0,0 +1,10 @@
+from time_calculator import add_time
+from unittest import main
+
+
+print(add_time("11:43 AM", "00:20"))
+# Returns: 12:03 PM)
+
+
+# Run test
+main(module='test_module', exit=False)
